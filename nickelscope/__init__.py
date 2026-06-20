870eb92afebc9d4e6946691d88ac809ab82df429
UlinNuha <mulinuhaa@gmail>
2026-06-20 08:31:12 +0700

    fix the module

--- nickelscope/__init__.py
+++ nickelscope/__init__.py
@@ -1,4 +1,3 @@
 from .geology import query_point, query_grid, classify_rock, get_geology_legend_html, ROCK_COLORS
 from .gee import init_gee, build_gee_features, get_geology_from_local
 from .ml import load_model, predict_grid, FEATS
-from .maps import build_map, add_geology_overlay, add_geology_legend, add_prediction_heatmap
